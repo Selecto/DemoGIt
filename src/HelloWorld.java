d5b0997b2c86507c5836f2292721038df9fe3e8e
Selecto <takialimat@MacBook-Pro-de-Taki-Alimat.local>
2015-01-29 21:26:54 -0500

    Rajouter un Hello World dans src/

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+import java.io.IOException;
+
+
+
+/**
+ *
+ * @author takialimat
+ */
+public class HelloWorld {
+     public static void main(String[] args) throws IOException{
+         System.out.println("blablabla");
+    
+}
